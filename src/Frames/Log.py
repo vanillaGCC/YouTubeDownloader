011cc34fef7270b6a290d949a0016a7dab416085
vanillaGCC <n.youshow68@gmail.com>
2023-07-16 20:46:11 +0900

    change directory

--- src/Frames/Log.py
+++ src/Frames/Log.py
@@ -1,11 +1,12 @@
 import wx
 import logging
 
+
 class LogFrame(wx.Frame):
     def __init__(self, *args, **kwargs):
         super(LogFrame, self).__init__(*args, **kwargs)
 
-        self.log_textctrl = wx.TextCtrl(self, style=wx.TE_MULTILINE|wx.TE_READONLY)
+        self.log_textctrl = wx.TextCtrl(self, style=wx.TE_MULTILINE | wx.TE_READONLY)
 
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(self.log_textctrl, 1, wx.EXPAND)
@@ -15,6 +16,7 @@ class LogFrame(wx.Frame):
         logger = logging.getLogger()
         logger.addHandler(LogHandler(self.log_textctrl))
 
+
 class LogHandler(logging.Handler):
     def __init__(self, log_output):
         super(LogHandler, self).__init__()
@@ -22,4 +24,4 @@ class LogHandler(logging.Handler):
 
     def emit(self, record):
         msg = self.format(record)
-        self.log_output.AppendText(f'{msg}\n')
+        self.log_output.AppendText(f"{msg}\n")
--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@
 import os
 import sys
 import wx
-from MyProject1MyFrame1Chiled import MyProject1MyFrame1Child
+from Frames.MyProject1MyFrame1Chiled import MyProject1MyFrame1Child
 
 if __name__ == "__main__":
     app = wx.App(False)
